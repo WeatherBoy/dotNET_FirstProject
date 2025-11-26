51624b13542613a8cbfc7315d3bede326cb0b086
WeatherBoy <dafloxi@gmail.com>
2025-11-26 11:23:51 +0100

    :construction: horrendous first exercise

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+string[] allOrders = ["B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"];
+
+const string exoticStartingCharacter = "B";
+
+foreach (var order in allOrders) {
+    if (order.StartsWith(exoticStartingCharacter)) {
+        Console.WriteLine(order);
+    }
+}
