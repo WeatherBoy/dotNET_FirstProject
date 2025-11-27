23e3b792ae3abeffd11ff37e8b31acc804eaae67
WeatherBoy <dafloxi@gmail.com>
2025-11-27 09:45:11 +0100

    :construction: update main to match new minor project

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -11,7 +11,24 @@ class Program
         
         
         Console.WriteLine("*** More in-depth array processing exercise ***");
+        string[] students = ["Sophia", "Andrew", "Emma", "Logan"];
+        int[][] allGrades =
+        [
+            [90, 86, 87, 98, 100],
+            [92, 89, 81, 96, 90],
+            [90, 85, 87, 98, 68],
+            [90, 95, 87, 88, 96]
+        ];
+        var allStudentsWithScores = new Dictionary<string, int[]>();
+        int numStudents = students.Length;
         
+        // populate `allScores`
+        for (int i = 0; i < numStudents; i++)
+        {
+            allStudentsWithScores.Add(students[i], allGrades[i]);
+        }
+
+        ProcessArrayData.PrintFormattedGradeReport(allStudentsWithScores);
     }
 }
 
